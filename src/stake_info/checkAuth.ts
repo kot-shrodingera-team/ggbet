b9cbce8253f91cf3e03a1c4db49a92674df01667
kot-shrodingera <kot.shrodingera@gmail.com>
2021-07-26 17:02:07 +0300

    fix(checkAuth): селекторы

--- src/stake_info/checkAuth.ts
+++ src/stake_info/checkAuth.ts
@@ -3,9 +3,9 @@ import checkAuthGenerator, {
 } from '@kot-shrodingera-team/germes-generators/stake_info/checkAuth';
 
 export const noAuthElementSelector =
-  '.header__betting-elements.header__betting-elements--no-login';
+  '.header__betting-elements.header__betting--hide, .header__betting-elements.header__betting-elements--no-login';
 export const authElementSelector =
-  '.header__betting-elements:not(.header__betting-elements--no-login)';
+  '.header__betting-elements:not(.header__betting--hide):not(.header__betting-elements--no-login)';
 
 export const authStateReady = authStateReadyGenerator({
   noAuthElementSelector,
