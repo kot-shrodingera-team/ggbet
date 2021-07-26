5989ecb5d64bb23185bcb051678a18d70e689ca8
kot-shrodingera <kot.shrodingera@gmail.com>
2021-07-26 17:06:01 +0300

    feat(authorize): решение капчи

--- src/initialization/authorize.ts
+++ src/initialization/authorize.ts
@@ -1,11 +1,34 @@
 import authorizeGenerator from '@kot-shrodingera-team/germes-generators/initialization/authorize';
+import {
+  getElement,
+  log,
+  resolveRecaptcha,
+} from '@kot-shrodingera-team/germes-utils';
 // import { authElementSelector } from '../stake_info/checkAuth';
 // import { updateBalance, balanceReady } from '../stake_info/getBalance';
 // import afterSuccesfulLogin from './afterSuccesfulLogin';
 
-// const setLoginType = async (): Promise<boolean> => {
+// const preInputCheck = async (): Promise<boolean> => {
 //   return true;
 // };
+
+const beforeSubmitCheck = async (): Promise<boolean> => {
+  const recaptchaIFrame = await getElement('iframe[title="reCAPTCHA"]', 1000);
+  if (recaptchaIFrame) {
+    log('Есть капча. Пытаемся решить', 'orange');
+    try {
+      await resolveRecaptcha();
+    } catch (e) {
+      if (e instanceof Error) {
+        log(e.message, 'red');
+      }
+      return false;
+    }
+  } else {
+    log('Нет капчи', 'steelblue');
+  }
+  return true;
+};
 
 const authorize = authorizeGenerator({
   openForm: {
@@ -15,13 +38,14 @@ const authorize = authorizeGenerator({
     // triesInterval: 1000,
     // afterOpenDelay: 0,
   },
-  // setLoginType,
+  // preInputCheck,
   loginInputSelector: 'input#_username',
   passwordInputSelector: 'input#_password',
   submitButtonSelector: '.popup-layout form button.btn-success',
   // inputType: 'fireEvent',
   fireEventNames: ['input'],
   // beforeSubmitDelay: 0,
+  beforeSubmitCheck,
   // captchaSelector: '',
   // loginedWait: {
   //   loginedSelector: authElementSelector,
