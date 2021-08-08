0a0146eb57c3d18a1a20e29097db8dd5a7ea7c7a
kot-shrodingera <kot.shrodingera@gmail.com>
2021-08-08 23:39:11 +0300

    fix(preOpenBet): ожидание вкладки купона пари 10 секунд, вместо 5

--- src/show_stake/preOpenBet.ts
+++ src/show_stake/preOpenBet.ts
@@ -3,7 +3,8 @@ import { JsFailError } from '@kot-shrodingera-team/germes-utils/errors';
 
 const preOpenBet = async (): Promise<void> => {
   const betslipTab = await getElement<HTMLElement>(
-    '.__app-Tabs-tab:nth-child(1)'
+    '.__app-Tabs-tab:nth-child(1)',
+    10000
   );
   if (!betslipTab) {
     throw new JsFailError('Не найдена вкладка "Купон Пари"');
