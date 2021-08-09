538ca27b0518c75b4428d2be319697c92f3b5901
kot-shrodingera <kot.shrodingera@gmail.com>
2021-08-09 19:07:45 +0300

    feat(preOpenBet): открывание боковой панели на узких окнах

--- src/show_stake/preOpenBet.ts
+++ src/show_stake/preOpenBet.ts
@@ -2,10 +2,23 @@ import { getElement, log } from '@kot-shrodingera-team/germes-utils';
 import { JsFailError } from '@kot-shrodingera-team/germes-utils/errors';
 
 const preOpenBet = async (): Promise<void> => {
-  const betslipTab = await getElement<HTMLElement>(
-    '.__app-Tabs-tab:nth-child(1)',
-    10000
+  const sibebarTogglerNotOpennedButtonSelector =
+    '[class*="sidebarToggler__container___"]:not([class*="sidebarToggler__is-open-sidebar___"]) > [class*="sidebarToggler__btn___"]';
+  const betlipTabSelector = '.__app-Tabs-tab:nth-child(1)';
+
+  await Promise.race([
+    getElement(sibebarTogglerNotOpennedButtonSelector, 10000),
+    getElement(betlipTabSelector, 10000),
+  ]);
+  const sideBarTogglerNotOpennedButton = document.querySelector<HTMLElement>(
+    sibebarTogglerNotOpennedButtonSelector
   );
+  if (sideBarTogglerNotOpennedButton) {
+    log('Открываем боковую панель', 'orange');
+    sideBarTogglerNotOpennedButton.click();
+    await getElement<HTMLElement>(betlipTabSelector, 2000);
+  }
+  const betslipTab = document.querySelector<HTMLElement>(betlipTabSelector);
   if (!betslipTab) {
     throw new JsFailError('Не найдена вкладка "Купон Пари"');
   }
